61c804090006ad29c4263aa13ae1897896495fb0
Steven Craig Moss <meanstackmaven@zoho.com>
2017-03-09 15:27:27 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+board = Array.new
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# board[0] = "top_left"
+# board[1] = "top_middle"
+# board[2] = "top_right"
+# board[3] = "mid- left"
+# board[4] = "mid_middle"
+# board[5] = "mid_right"
+# board[6] = "bottom_left"
+# board[7] = "bottom_middle"
+# board[8] = "bottom_right"
